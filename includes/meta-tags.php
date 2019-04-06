990bd2214913f2c2d9fa85f4cc1aeca1ee09f3ea
Jack Greenberg <j.lester.greenberg@gmail.com>
2019-04-05 22:11:58 -0700

    Add SEO stuff

---
+++ includes/meta-tags.php
@@ -0,0 +1,11 @@
+<meta name="description" content="Photo blog by Jack Greenberg.">
+<meta itemprop="name" content="Jack Greenberg | Photos">
+<meta itemprop="description" content="Photo blog by Jack Greenberg.">
+<meta property="og:url" content="https://photos.jacklgreenberg.com">
+<meta property="og:type" content="website">
+<meta property="og:title" content="Jack Greenberg | Photos">
+<meta property="og:description" content="Photo blog by Jack Greenberg.">
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:title" content="Jack Greenberg | Photos">
+<meta name="twitter:description" content="Photo blog by Jack Greenberg.">
+<meta name="google-site-verification" content="94WLxdgfjqv6mwx8FHYGc3UXUMCyxyunjy_daFH0Dls" />
