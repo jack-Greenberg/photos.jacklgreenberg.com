b893f67d0a2ec9fee63f90f18f60e8c3321a9fb1
Jack Greenberg <j.lester.greenberg@gmail.com>
2019-04-05 22:12:40 -0700

    Update includes and styling

--- includes/head.php
+++ includes/head.php
@@ -3,13 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>
-        <?php
-            // This allows us to change the title of the blog
-            // from the config file
-            echo $ms->render('{{ title }}', array('title' => $title));
-        ?>
-    </title>
+    <title><?php echo $ms->render('{{ title }}', array('title' => $title)); ?></title>
 
     <?php include(dirname(__FILE__).'/meta-tags.php') ?>
 
